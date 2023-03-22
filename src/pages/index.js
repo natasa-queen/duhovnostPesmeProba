6932c5429d40fc0306eccccf50060bd6d0b1315b
A.M <andrej2000aa@gmail.com>
2023-03-22 13:28:17 +0100

    novi

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import * as React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <h1>Zdravo svete!</h1>
+      <h2>Zdravo Natasa!</h2>
+    </>
+  )
 }
