8fd7033af71a700092e5ec86ade75f981a267f2c
A.M <andrej2000aa@gmail.com>
2023-03-23 22:30:00 +0100

    ujutro post

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
             <HeaderSlika />
 
             <div>
-                <Poruka hour={22} minute={29} />
+                <Poruka hour={7} minute={29} />
             </div>
 
             
