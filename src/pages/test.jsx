0c1bb5cfd6ec5920dfb21a04e1a78631c5fcbac4
A.M <andrej2000aa@gmail.com>
2023-05-14 11:48:23 +0200

    sa webstorm

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import SpisakPesama from '../components/SpisakPesama'
-
-
-
-export default function NovaPesma() {
-    return (
-        <>
-            <h1>test strana</h1>
-
-            <SpisakPesama />
-        </>
-    )
-}
+// import React from 'react'
+// import SpisakPesama from '../components/SpisakPesama'
+//
+//
+//
+// export default function NovaPesma() {
+//     return (
+//         <>
+//             <h1>test strana</h1>
+//
+//             <SpisakPesama />
+//         </>
+//     )
+// }
